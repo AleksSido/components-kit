36d8727adf14c67a8badf4bf5b2b9493fec3ab72
asid <asid@payneteasy.com>
2022-08-09 07:31:58 +0300

    update repo

---
+++ component-library-react/src/components/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'stencil-components';
+
+import { defineCustomElements } from 'stencil-components/loader';
+
+defineCustomElements();
+export const StencilInput = /*@__PURE__*/createReactComponent<JSX.StencilInput, HTMLStencilInputElement>('stencil-input');
